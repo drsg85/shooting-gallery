9444b7d45465bf0a72994ef1a316532d908c8968
bagel.r <Bagel.R@citilink.ru>
2022-12-19 10:07:32 +0300

    Add some sections and features

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,28 @@
 'use strict';
+import { tns } from '../../node_modules/tiny-slider/src/tiny-slider.js';
 import MobileMenu from './modules/MobileMenu';
-import Service from './modules/service';
+import './modules/service';
 import Modal from './modules/modal';
+
 // import ToTop from './modules/toTop';
 
 const mobileMenu = new MobileMenu();
 const modal = new Modal();
-const service = new Service();
+// const service = new Service();
 // new ToTop(); 
+
+if(document.querySelector('.about__slider')) {
+  const slider = new tns({
+      container: '.slider',
+      mode: "carousel",
+      controls: false,
+      nav: false,
+      items: 3,
+      autoplay: true,
+      autoplaySpeed: 0,
+      speed: 2000,
+      arrows: false,
+      swipe: false,
+      autoplayButtonOutput: false,
+    });
+}
