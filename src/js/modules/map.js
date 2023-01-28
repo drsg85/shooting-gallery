7a7059684cdd70de60abceda130fd21c6c4b193f
bagel.r <Bagel.R@citilink.ru>
2023-01-28 12:02:49 +0300

    add yandex map

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,25 +1,25 @@
-const map = L.map('map')
-  .on('load', () => {
-  })
-  .setView({
-    lat: 54.710641,
-    lng: 20.485406,
-  }, 16);
+// const map = L.map('map')
+//   .on('load', () => {
+//   })
+//   .setView({
+//     lat: 54.710641,
+//     lng: 20.485406,
+//   }, 16);
 
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(map);
+// L.tileLayer(
+//   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+//   {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//   },
+// ).addTo(map);
 
-const marker = L.marker(
-    {
-      lat: 54.710641,
-      lng: 20.485406,
-    },
-  );
+// const marker = L.marker(
+//     {
+//       lat: 54.710641,
+//       lng: 20.485406,
+//     },
+//   );
 
-  marker.addTo(map);
+//   marker.addTo(map);
 
-  marker.bindPopup("ТИР39").openPopup();
+//   marker.bindPopup("ТИР39").openPopup();
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,7 @@ import { tns } from '../../node_modules/tiny-slider/src/tiny-slider.js';
 import MobileMenu from './modules/MobileMenu';
 import './modules/service';
 import './modules/scroll';
-import './modules/map';
+// import './modules/map';
 import Modal from './modules/modal';
 
 import ToTop from './modules/toTop';
@@ -40,7 +40,8 @@ if(document.querySelector('.about__slider')) {
           items: 3
         },
         1400: {
-          items: 4
+          items: 4,
+          gutter: 20
         }
       }
     });
